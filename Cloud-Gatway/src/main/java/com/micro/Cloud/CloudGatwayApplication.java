fa537c860422be905c503ead864ef8fe7de63d88
10714851 <ravi.sannu0032@gmail.com>
2023-06-01 11:49:24 +0530

    Cloud Gatway application

---
+++ Cloud-Gatway/src/main/java/com/micro/Cloud/CloudGatwayApplication.java
@@ -0,0 +1,19 @@
+package com.micro.Cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class CloudGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatwayApplication.class, args);
+	}
+
+
+}
